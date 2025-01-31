1b21971abba141d7eeaf1fc9c69cff30c0ea56a9
Savannah <savannah_designs@icloud.com>
2025-01-30 17:15:03 -0800

    Create ConfrimedBooking.js

---
+++ ConfrimedBooking.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const ConfrimedBooking = () => {
+    return(
+        <div className='confirm'>
+        <div>
+        <h1>Booking has been <span>confimed</span> </h1>
+        </div>
+        </div>
+
+    );
+};
+
+export default ConfrimedBooking;
